5b7d3bf77fb34a97df4b57e8dc8b9b05c4c866c9
shell <shell@thisworld.club>
2025-03-04 01:58:05 +0800

    new data

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const { userVerification } = require("./utils/userverif");
 
 async function main() {
   try {
-    const fileContent = await readFile("Template.md");
-    console.log("File content:", fileContent);
+  //  const fileContent = await readFile("data/test.json");
+   // console.log("File content:", fileContent);
     await userVerification()
    // await updateFile("file.txt", "New content", "Update file content");
   } catch (error) {
